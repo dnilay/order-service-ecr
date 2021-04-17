5cb2d53cb3848f75d1ec5e9776b17e4b431c89bd
Nilay Das <mailnilaydas@gmail.com>
2021-04-17 12:55:47 +0530

    initial commit

---
+++ src/test/java/com/example/orderservicecodebuildecr/OrderServiceCodebuildEcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderservicecodebuildecr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceCodebuildEcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
